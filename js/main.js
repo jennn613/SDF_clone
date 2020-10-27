4e0013be76f059931de88376ed10f7acf11a9b97
Jenny <jennn613@gmail.com>
2020-10-27 16:09:22 +0900

    move files

---
+++ js/main.js
@@ -0,0 +1,94 @@
+"use strict";
+
+//to the top
+const topBtnParent=document.querySelector('.fixed_btn'); //우측 하단 고정 버튼 감싼 박스
+const topBtn= topBtnParent.querySelectorAll('button')[1] //우측 하단 고정 버튼 중 두번째 버트
+topBtn.addEventListener('click', ()=>{
+window.scrollTo({top:0, left:0, behavior:'smooth'});
+});
+
+//showing menu items when move the mouse on the header
+const header= document.querySelector('.header_nav_box'); //header 감싼 박스
+
+const headerMenu=document.querySelector('.header_nav_menu_list_item'); //소메뉴
+const navItems= document.querySelectorAll('.header_nav_menu_list_item');
+const showMenu= headerMenu.classList.contains('hidden');
+
+const headerLeftSide= document.querySelector('.header_nav_left'); //gnb 왼쪽 박스
+const headerLogo = headerLeftSide.querySelectorAll('i')[0]; //gnb 로고
+const headerLeftText = document.querySelector('.header_nav_left_text'); //왼쪽 박스의 텍스트 감싼 박스
+const headerLangSet = headerLeftText.querySelectorAll('span'); // 언어 설정 텍스트
+const headerMenuListBox = document.querySelector('.header_nav_menu_list'); //메뉴 ul
+const headerMenuList = headerMenuListBox.querySelectorAll('li');
+const headerBtn = document.querySelector('.nav_btn_signIn');
+
+function changeNavColor(){ //nav 색 바꿔주는 함수
+    header.style.backgroundColor = 'white';
+    headerLogo.style.color = '#0082c6';
+    headerLangSet.forEach(function(langSet){
+        langSet.style.color = 'black';});
+             // headerLangSet.style.color = 'black';
+        // function ()안에 들어가는 이름은 상관X
+    headerMenuList.forEach(function (headerList){ headerList.style.color = 'black';});
+    headerBtn.style.borderColor = '#003492';
+    
+}
+
+function originalColor() { //원래 nav의 색
+    headerLogo.style.color = 'white';
+    headerLangSet.forEach(function(langSet){
+        langSet.style.color = 'white';});
+    headerMenuList.forEach(function (headerList){ headerList.style.color = 'white';});
+    headerBtn.style.borderColor = 'transparent';
+    header.style.backgroundColor = 'transparent';
+}
+
+
+//스크롤 내리면 네비게이션에 배경색 생기기
+const headerHeight  = header.getBoundingClientRect().height;
+   document.addEventListener('scroll', ()=> {
+        if(window.scrollY > headerHeight)
+        {
+          header.classList.add('headercolor');
+          changeNavColor();
+
+        } 
+        else {
+            header.classList.remove('headerColor');
+     originalColor();
+    }
+    });
+
+
+//메뉴 보이기 & 숨기기
+
+const headerMenuHeight = headerMenu.getBoundingClientRect().height;
+
+header.addEventListener('mouseenter', () => {
+
+    navItems.forEach(function(list){
+        list.classList.remove('hidden');
+    });  
+    setTimeout(function(){headerMenuHeight='auto';},10);
+    header.style.backgroundColor = 'white';
+   changeNavColor();
+
+    
+});
+
+
+header.addEventListener('mouseleave', () => {
+    navItems.forEach(function (navhide){
+        navhide.classList.add('hidden');
+    });
+    originalColor();
+});
+
+
+
+    // headerLangSet.forEach(function(langSet){
+    //     langSet.addEventListener('click',function(){
+    //         langSet.style.fontWeight = "700";
+    //     }); 
+    // });
+    
